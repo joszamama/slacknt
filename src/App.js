4a69ea8245ed0a8c04e8e8bfef9e4c505fe0755e
José Antonio Zamudio <joseantoniozamudio11@gmail.com>
2021-02-21 23:22:30 +0100

    set routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './includes/css/App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
       <header className="app-header">
-       <h1>Building Slacknt</h1>
+        {/* React router allow us to have different pages into our website*/}
+        <Router>
+
+          <Switch>
+            <Route path="/" exact>
+              <h1> This is the homepage</h1>
+            </Route>
+          </Switch>
+
+        </Router>
       </header>
     </div>
   );
