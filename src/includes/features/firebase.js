f0cbeda53f687a13a90722b6e516c18ddb3f4bb0
José Antonio Zamudio <joseantoniozamudio11@gmail.com>
2021-02-21 23:14:33 +0100

    Setting up the project

---
+++ src/includes/features/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCpC4a_68dW6bD9m4A-Le_pbUQIdV-CRFs",
+    authDomain: "slacknt-e8517.firebaseapp.com",
+    projectId: "slacknt-e8517",
+    storageBucket: "slacknt-e8517.appspot.com",
+    messagingSenderId: "495860617235",
+    appId: "1:495860617235:web:cf75600783facf280aefe6"
+};
